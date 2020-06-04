9cd0b8b6b23e5d50ff6217546f0d9a28df009ae9
matino06 <matino0546@gmil.com>
2020-06-04 09:27:02 +0200

    fine

--- src/components/house-rules/house-rules.jsx
+++ src/components/house-rules/house-rules.jsx
@@ -5,17 +5,17 @@ import './house-rules.scss'
 //import GridImg from '../grid-img/grid-img'
 
 const HouseRules = () => (
-    <div class="popup" id="popup">
-        <div class="popup__content">
-            <div class="popup__left">
-                <img src="./assets/dnevni.jpg" alt="Tour photo" class="popup__img"/>
-                <img src="./assets/vrt.jpg" alt="Tour photo" class="popup__img"/>
+    <div className="popup" id="popup">
+        <div className="popup__content">
+            <div className="popup__left">
+                <img src="./assets/dnevni.jpg" alt="Tour photo" className="popup__img"/>
+                <img src="./assets/vrt.jpg" alt="Tour photo" className="popup__img"/>
             </div>
-            <div class="popup__right">
-                <a href="#" class="popup__close">&times;</a>
-                <h2 class="heading-secondary ">House rules</h2>
-                <h3 class="heading-tertiary u-margin-bottom-small">Important &ndash; Please read these terms before booking</h3>
-                <ul class="popup__text u-margin-bottom-small">
+            <div className="popup__right">
+                <a href="#" className="popup__close">&times;</a>
+                <h2 className="heading-secondary ">House rules</h2>
+                <h3 className="heading-tertiary u-margin-bottom-small">Important &ndash; Please read these terms before booking</h3>
+                <ul className="popup__text u-margin-bottom-small">
                     <li>Check-in 3:00 PM - 10:00 PM</li>
                     <li>Check-out 7:00 AM - 10:00 AM</li>
                     <li>A EUR200 damage deposit is required on arrival. This deposit will be fully refunded at check-out, as long as there isn't any damage to the property.</li>
@@ -27,7 +27,7 @@ const HouseRules = () => (
                     <li>Pets are not allowed.</li>
                     <li>Child policies: Crib by request (Free)</li>
                 </ul>
-                <a href="#" class="btn-popup btn-popup--blue">Book now</a>
+                <a href="#" className="btn-popup btn-popup--blue">Book now</a>
             </div>
         </div>
     </div>
